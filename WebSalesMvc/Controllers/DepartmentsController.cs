eb2ca54116b2c081436b7d183cd8450be438084a
Thiago Ferc <thiago@thiagoferreira.net>
2023-10-18 16:16:44 -0300

    created departments model and departments controller - added view departments

---
+++ WebSalesMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSalesMvc.Models;
+
+namespace WebSalesMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departments> list = new List<Departments>();
+            list.Add(new Departments { Id = 1, Name = "Eletronics" });
+            list.Add(new Departments { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
